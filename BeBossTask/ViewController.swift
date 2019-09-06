b9c9b2687d13739790adb6da24929fd93fbcda3f
Nikita <luzhbinnikita@gmail.com>
2019-09-06 12:34:27 +0300

    Initial

---
+++ BeBossTask/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BeBossTask
+//
+//  Created by Никита Лужбин on 06/09/2019.
+//  Copyright © 2019 Никита. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
